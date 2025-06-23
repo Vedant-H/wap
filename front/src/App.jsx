3cc0f4cbe2a5afe13f0f536ad0bd9d7d0c67d2dc
Vedant <your-new-email@example.com>
2025-06-23 16:48:14 +0530

    added signup page

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
+import SignPage from "./pages/SignPage";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<HomePage/>} />
+      <Route path="/signin" element={<SignPage/>}/>
     </Routes>
   </BrowserRouter>
     </>
