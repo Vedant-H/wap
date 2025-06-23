03e65dee6947f412d77a103b93da2dcec7e636db
Vedant <your-new-email@example.com>
2025-06-23 16:44:57 +0530

    added login page

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<HomePage/>} />
+      <Route path="/login" element={<LoginPage/>}/>
     </Routes>
   </BrowserRouter>
     </>
