7ee8d6e7f4b65a3fa3dd386612cc0e68ad7a5934
Vedant <your-new-email@example.com>
2025-06-23 16:58:05 +0530

    added header

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,11 +4,13 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import SignPage from "./pages/SignPage";
+import Header from "./components/Header";
 
 function App() {
 
   return (
     <>
+    <Header/>
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<HomePage/>} />
